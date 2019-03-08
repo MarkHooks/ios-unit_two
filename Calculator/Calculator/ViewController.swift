7ca835fa6f1b9e15d8a0c8e8e946a8b3738f1bf0
Mark <mark@iMac2.local>
2019-03-08 09:18:16 -0500

    created the best dang calculator in town

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Mark on 3/7/19.
+//  Copyright © 2019 Mark. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
